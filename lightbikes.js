e2e9672f5f93c23234861d866a30c79a139fdcd3
BioNinja45 <dabob45@gmail.com>
2016-08-29 15:26:32 -0500

    Add files via upload

--- lightbikes.js
+++ lightbikes.js
@@ -1,11 +1,15 @@
 var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = "#FF0000"
+var backCanvas = document.createElement('canvas');
+backCanvas.height =canvas.height;
+backCanvas.width = canvas.width;
+var backCtx = backCanvas.getContext('2d');
 
 var speed = 1/16000;
 
 var x = 0;
 var y = 0;
+
 
 var input = {
 	up:false,
@@ -64,11 +68,33 @@ window.onkeyup = function(event)
 
 function loop()
 {
-		if(input.up)y-=1;
-		if(input.down)y+=1;
-		if(input.left)x-=1;
-		if(input.right)x+=1;
-		ctx.fillRect(x,y,5,5);
-		setTimeout(loop,speed);
+		if(input.up)y-=2;
+		if(input.down)y+=2;
+		if(input.left)x-=2;
+		if(input.right)x+=2;
+		
+		backCtx.clearRect(0,0,canvas.width,canvas.height);
+		
+		for(i = 0; i< 100; i++){
+			backCtx.fillStyle = "blue";
+			backCtx.fillRect(
+			i * 5, 
+			i * 5,
+			10,
+			10);
+		}
+		
+		backCtx.fillStyle = "#FF0000"
+		backCtx.fillRect(x,y,5,5);
+		
+		ctx.drawImage(x,y,5,5);
+		requestAnimationFrame(loop);
 }
-loop();
+
+//var intervalId = setInterval(loop,speed);
+
+requestAnimationFrame(loop);
+
+
+
+
